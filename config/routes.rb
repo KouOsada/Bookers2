242c556feeea4ad58d224b2085d8259d9666a832
KouOsada <kouosada.0930@gmail.com>
2021-09-12 12:21:48 +0000

    [Add]Aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:index, :show]
+  get 'about' => 'homes#about', as: 'about' 
 
 end
