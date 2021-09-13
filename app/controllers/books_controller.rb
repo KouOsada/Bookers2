3fed7756ad7acd8fe0d53f830fd2150310f39875
KouOsada <kouosada.0930@gmail.com>
2021-09-13 05:51:59 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,20 @@
 class BooksController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
-  
+
 end
