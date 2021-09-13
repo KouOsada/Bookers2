cb7dcdcac250ce9a4c6bc318f7bea195bd785e2d
KouOsada <kouosada.0930@gmail.com>
2021-09-13 07:53:10 +0000

    [Update]投稿一覧画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,27 @@
 class BooksController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
-
   def index
     @book = Book.new
     @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 
 end
