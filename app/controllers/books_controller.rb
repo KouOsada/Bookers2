bb506d5d27165d9df8d61986fee2eeea71848bdc
KouOsada <kouosada.0930@gmail.com>
2021-09-14 04:33:38 +0000

    [Add]編集画面・機能・更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def show
@@ -20,6 +20,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
