240cddbdad50eba672188bf8ed42250582b9c71e
KouOsada <kouosada.0930@gmail.com>
2021-09-19 06:52:57 +0000

    [Update]各種テンプレートの追加・ビューファイルの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,5 @@
 class BooksController < ApplicationController
-  before_action :correct_user, only: [:edit, :update]
-
-  def index
-    @user = current_user
-    @book = Book.new
-    @books = Book.all
-  end
-
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-
-    if
-      @book.save
-      flash[:success] = "You have created book successfully."
-      redirect_to book_path(@book)
-    else
-      @user = current_user
-      @books = Book.all
-      render :index
-    end
-
-  end
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @books = Book.new
@@ -29,21 +7,35 @@ class BooksController < ApplicationController
     @user = @book.user
   end
 
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
   def edit
     @book = Book.find(params[:id])
   end
 
+
+
   def update
     @book = Book.find(params[:id])
-
-    if
-      @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book)
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render :edit
+      render "edit"
     end
-
   end
 
   def destroy
@@ -58,7 +50,7 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-  def correct_user
+  def ensure_correct_user
     @book = Book.find(params[:id])
     @user = @book.user
     redirect_to(books_path) unless @user == current_user
