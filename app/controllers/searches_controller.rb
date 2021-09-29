95e03511f77013c8265b19d6a21f6472cffe2421
KouOsada <kouosada.0930@gmail.com>
2021-09-29 07:38:19 +0000

    [Update]controllers

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,65 +1,14 @@
 class SearchesController < ApplicationController
-  
+
   def search
-    @model = params["model"]
-    @content = params["content"]
-    @method = params["method"]
-    @records = serach_for(@model, @content, @method)
-  end
-  
-  private
-  
-  def match(model, content)
-    if 
-      model == 'user'
-      User.where(name: content)
-    elsif
-      model == 'book'
-      Book.where(title: content)
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+
+    if @model == 'user'
+      @records = User.serach_for(@content, @method)
+    else
+      @records = Book.search_for(@content, @method)
     end
   end
-  
-  def forward(model, content)
-    if 
-      model == 'user'
-      User.where("name LIKE ?", "#{content}%")
-    elsif
-      model == 'book'
-      Book.where("title LIKE ?", "#{content}%")
-    end
-  end
-  
-  def backward(model, content)
-    if
-      model == 'user'
-      User.where("name LIKE ?", "%#{content}")
-    elsif
-      model == 'book'
-      Book.where("title LIKE ?", "%#{content}")
-    end
-  end
-  
-  def partial(model, content)
-    if
-      model == 'user'
-      User.where("name LIKE ?", "%#{content}%")
-    elsif
-      model == 'book'
-      Book.where("title LIKE ?", "%#{content}%")
-    end
-  end
-  
-  def serach_for(model, content, method)
-    case method
-    when 'match'
-      match(model, content)
-    when 'forward'
-      forward(model, content)
-    when 'backward'
-      backward(model, content)
-    when 'partial'
-      partial(model, content)
-    end
-  end
-  
 end
