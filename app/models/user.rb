944f148f367ffcc358a04e6f938e973f1bedd492
KouOsada <kouosada.0930@gmail.com>
2021-10-04 06:01:05 +0000

    [Add]いいねの期間範囲

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :favorited_books, through: :favorites, source: :book
   has_many :book_comments, dependent: :destroy
   
   # フォロー機能に関連
